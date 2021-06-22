aa17802cfd23d4eaa588ae4b2d8f4a8ee239b549
Andrii Aleksandrov <=>
2021-06-22 20:23:05 +0300

    Add swagger API documentation

--- fininstitution/urls.py
+++ fininstitution/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
+    path('openapi/', get_schema_view(
+        title="Financial Institution API",
+        description="API for financial institution",
+        version="1.0.0",
+        urlconf='accounts.urls'
+    ), name='openapi-schema'),
+    path('', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
 ]
