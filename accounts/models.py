ea8b254178e2aaceb038386d3f692d8780ccb9b0
Andrii Aleksandrov <00000001xyz@gmail.com>
2021-06-22 14:47:49 +0300

    Add account and account action models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class BankAccount(models.Model):
+    """Store a single bank account entry for the user"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class BalanceAction(models.Model):
+    """Store a single balance action entry of bank account history"""
+    bank_account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
+    performed_at = models.DateTimeField(auto_now_add=True, editable=False)
+    message = models.TextField()
