fce677418898b35accc089f7ab9ed1d41b343bae
Andrii Aleksandrov <=>
2021-06-23 11:35:54 +0300

    Allow superusers to create staff users

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -22,9 +22,28 @@ class BankAccountSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'id',)
-
+        fields = ('url', 'username', 'id', 'password', 'is_staff')
         read_only_fields = ('id',)
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_staff': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+        is_staff = False
+        if user.is_superuser:
+            is_staff = validated_data.get('is_staff', False)
+        user = User.objects.create(
+            username=validated_data['username'],
+            is_staff=is_staff
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class BalanceActionSerializer(serializers.ModelSerializer):
