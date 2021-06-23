50f0500f4c4396f3f09806ec1578790c20005cd5
Andrii Aleksandrov <=>
2021-06-23 13:34:09 +0300

    Add JWT auth

--- fininstitution/urls.py
+++ fininstitution/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('openapi/', get_schema_view(
@@ -16,5 +21,8 @@ urlpatterns = [
         extra_context={'schema_url': 'openapi-schema'}
     ), name='swagger-ui'),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include('accounts.urls')),
 ]
