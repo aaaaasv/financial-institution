16287ca496c33684c8ec60d2d0768c688794b6ce
Andrii Aleksandrov <=>
2021-06-24 14:27:59 +0300

    Wrap money transfer in the transaction

--- accounts/services.py
+++ accounts/services.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from django.db import transaction, connection
 from rest_framework import status
 
 from accounts.models import (
@@ -30,10 +31,11 @@ def transfer_money(from_account_pk, to_account_pk, amount):
     if amount > available_balance:
         return 'Not enough money available', status.HTTP_403_FORBIDDEN
 
-    new_from_balance = update_account_balance(from_account, -amount)
-    new_to_balance = update_account_balance(to_account, amount)
+    with transaction.atomic():
+        new_from_balance = update_account_balance(from_account, -amount)
+        new_to_balance = update_account_balance(to_account, amount)
 
-    add_balance_action(from_account, f'Withdrawn {amount} for {to_account.user.username}#{to_account.id}')
-    add_balance_action(to_account, f'Deposited {amount} by {from_account.user.username}#{from_account.id}')
+        add_balance_action(from_account, f'Withdrawn {amount} for {to_account.user.username}#{to_account.id}')
+        add_balance_action(to_account, f'Deposited {amount} by {from_account.user.username}#{from_account.id}')
 
     return {'New transferer balance': new_from_balance, 'New transferee balance': new_to_balance}, status.HTTP_200_OK
